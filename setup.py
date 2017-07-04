4efa82be53d56688a13810ce77559128901c3b3b
Phoenix Zerin <phoenix.zerin@eflglobal.com>
2017-07-04 16:45:49 +1200

    Bumping version number.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     description = 'Adds filters for interpreting ISO codes.',
     url         = 'https://filters.readthedocs.io/',
 
-    version = '1.0.1',
+    version = '1.0.2',
 
     packages = ['filters_iso'],
 
